a25e5a81784c382551d5bdff920abc34d1a23820
Enthazy <stephenx.wang@mail.utoronto.ca>
2021-02-10 17:19:47 -0500

    Add the vectorized model and an interface for jupyter

--- ulti.py
+++ ulti.py
@@ -15,6 +15,9 @@ def calculate_c(x1, y1, x2, y2, s1, s2):
     e = calculate_energy(x1, y1, x2, y2, s1, s2)
     return np.exp(np.divide(-1 * 4 * np.pi * e, s1**2))
 
+def calculate_lambd(x1, y1, x2, y2, s1, s2):
+    return np.divide(s2, s1)
+
 
 def calculate_nu(x1, y1, x2, y2, s1, s2):
     lambd = np.divide(s2, s1)
@@ -25,7 +28,7 @@ def calculate_t(x1, y1, x2, y2, s1, s2):
     c = calculate_c(x1, y1, x2, y2, s1, s2)
     v = calculate_nu(x1, y1, x2, y2, s1, s2)
     lambd = np.divide(s2, s1)
-    return c * np.power(v, (1 + np.square(lambd)))
+    return c * v * np.power(np.abs(v), (np.square(lambd)))
 
 
 def calculate_distance_sq(x1, y1, x2, y2):
@@ -34,6 +37,7 @@ def calculate_distance_sq(x1, y1, x2, y2):
 
 def show_message(x1, y1, x2, y2, s1, s2):
     print("energy: ", calculate_energy(x1, y1, x2, y2, s1, s2))
+    print("C: ", calculate_c(x1, y1, x2, y2, s1, s2))
     print("Nu: ", calculate_nu(x1, y1, x2, y2, s1, s2))
     print("T: ", calculate_t(x1, y1, x2, y2, s1, s2))
 
@@ -46,9 +50,9 @@ def check_difference(x1, x2):
 def gen_plot(x1_history, y1_history, x2_history, y2_history, xl=-6, xr=6, y=5):
     plt.plot(x1_history, y1_history, label="left")
     plt.plot(x2_history, y2_history, label="right")
-    plt.plot(np.arange(-5, 6), np.arange(-5, 6) * 0, label="0")
+    # plt.plot(np.arange(-5, 6), np.arange(-5, 6) * 0, label="0")
     # plt.xlim(xl, xr)
-    # plt.ylim(0, y)
+    plt.ylim(bottom=0)
     plt.legend(loc=1)
     # plt.title(str(np.round(tt, 3)))
     # plt.savefig("trail3_" + str(np.round(tt, 3)) + ".png")
@@ -58,17 +62,128 @@ def gen_plot(x1_history, y1_history, x2_history, y2_history, xl=-6, xr=6, y=5):
     # plt.close()
 
 
-def save_fig(x1_history, y1_history, x2_history, y2_history, name, xl=-6, xr=6, y=5):
+def save_fig_old(x1_history, y1_history, x2_history, y2_history, name, xl=-6, xr=6, y=5):
     plt.plot(x1_history, y1_history, label="left")
     plt.plot(x2_history, y2_history, label="right")
-    plt.plot(np.arange(-5, 6), np.arange(-5, 6) * 0, label="0")
+    plt.plot(np.arange(-3, 6), np.arange(-3, 6) * 0, label="0")
     # plt.xlim(xl, xr)
     # plt.ylim(0, y)
     plt.legend(loc=1)
     plt.title(name)
     plt.savefig(name + ".png")
+    # plt.show()
+    plt.show(block=False)
+    plt.pause(2)
+    plt.close()
+
+def gen_plot_forward(x1_history, y1_history, x2_history, y2_history, xl=-6, xr=6, y=5):
+    plt.figure(figsize=(8, 5))
+    plt.plot(x1_history, y1_history, c='coral', label="vortex 1, forward")
+    plt.plot(x2_history, y2_history, c='teal', label="vortex 2, forward")
+    # plt.plot(np.arange(-5, 6), np.arange(-5, 6) * 0, label="0")
+    plt.legend(loc=1)
+
+
+def gen_plot_backward(x1_history, y1_history, x2_history, y2_history, xl=-6, xr=6, y=5):
+    plt.plot(x1_history, y1_history, c='coral', linestyle="--", label="vortex 1, past")
+    plt.plot(x2_history, y2_history, c='teal', linestyle="--",label="vortex 2, past")
+    # plt.plot(np.arange(-5, 6), np.arange(-5, 6) * 0)
+    plt.legend(loc=1)
+
+
+def show_fig(lambd, value):
+    lambd = str(round(lambd, 2))
+    K = str(round(value, 3))
+    name = lambd + "_" + K
+    plt.title(r'$\lambda$'+" = "+ lambd + ", K = " + K, y=-0.13)
+    plt.ylim(0)
     plt.show()
 
+def save_fig(lambd, value, left = 1, right = -1):
+    lambd = str(round(lambd, 2))
+    K = str(round(value, 3))
+    name = lambd + "_" + K
+    plt.title(r'$\lambda$'+" = "+ lambd + ", K = " + K, y=-0.13)
+    plt.ylim(0)
+    if right != -1: plt.xlim(left, right)
+    plt.savefig(name + ".png")
+    plt.show()
+    # plt.show(block=False)
+    # plt.pause(1)
+    # plt.close()
 
-def save_data():
-    return
+
+# Refactor in vector
+
+def calculate_energy_vec(pos):
+    x1, y1, s1 = pos[0]
+    x2, y2, s2 = pos[1]
+    return calculate_energy(x1, y1, x2, y2, s1, s2)
+
+
+def calculate_p(pos):
+    y_coord = pos[:, 1]
+    s_lst = pos[:, 2]
+    return y_coord @ s_lst
+
+
+def calculate_c_vec(pos):
+    x1, y1, s1 = pos[0]
+    x2, y2, s2 = pos[1]
+    e = calculate_energy(x1, y1, x2, y2, s1, s2)
+    return np.exp(np.divide(-1 * 4 * np.pi * e, s1 ** 2))
+
+def calculate_lambd_vec(pos):
+    x1, y1, s1 = pos[0]
+    x2, y2, s2 = pos[1]
+    return s2/s1
+
+
+def calculate_w(pos):
+    c = calculate_c_vec(pos)
+    p = calculate_p(pos)
+    lambd = calculate_lambd_vec(pos)
+    s = pos[0][2]
+    w = np.power(p/s, 1+lambd*lambd)*c
+    return w
+
+def calculate_expression(pos):
+    x = calculate_lambd_vec(pos)
+    sq = 2*x+np.sqrt(4*x*x+1)
+    num = np.power(x+sq, 1+x*x)*np.power(1-sq,2*x)
+    dom = np.power(2,1+x*x)*(sq)*np.power(1+sq,2*x)
+    return num/dom
+
+def calculate_t_vec(pos):
+    x1, y1, s1 = pos[0]
+    x2, y2, s2 = pos[1]
+    c = calculate_c(x1, y1, x2, y2, s1, s2)
+    v = calculate_nu(x1, y1, x2, y2, s1, s2)
+    lambd = np.divide(s2, s1)
+    return c * v * np.power(np.abs(v), (np.square(lambd)))
+
+
+
+def gen_plot_forward_vec(history):
+    x1_history = history[:, 0, 0]
+    y1_history = history[:, 0, 1]
+    x2_history = history[:, 1, 0]
+    y2_history = history[:, 1, 1]
+    gen_plot_forward(x1_history, y1_history, x2_history, y2_history)
+
+
+def gen_plot_backward_vec(history):
+    x1_history = history[:, 0, 0]
+    y1_history = history[:, 0, 1]
+    x2_history = history[:, 1, 0]
+    y2_history = history[:, 1, 1]
+    gen_plot_backward(x1_history, y1_history, x2_history, y2_history)
+
+def show_fig_vec(lambd, w, value):
+    lambd = str(round(lambd, 2))
+    k = str(round(value, 3))
+    w = str(round(w, 3))
+    name = lambd + "_" + k
+    plt.title(r'$\lambda$'+" = " + lambd + ", W = " + w + ", bifur. = " + k, y=-0.13)
+    plt.ylim(0)
+    plt.show()
---
+++ vortex_system.py
@@ -0,0 +1,118 @@
+from ulti import *
+from ipywidgets import interactive, fixed
+
+def grad_euler_method(system):
+    x1, y1, s1 = system[0]
+    x2, y2, s2 = system[1]
+    dx1 = -1 * s2 * np.divide((y1 - y2), calculate_distance_sq(x1, y1, x2, y2)) \
+          + (-1 * (-1 * s1) * np.divide(1, 2 * y1)) \
+          + (-1 * (-1 * s2) * np.divide((y1 + y2), calculate_distance_sq(x1, y1, x2, -1 * y2)))
+
+    dy1 = 1 * s2 * np.divide((x1 - x2), calculate_distance_sq(x1, y1, x2, y2)) \
+          + ((-1 * s2) * np.divide((x1 - x2), calculate_distance_sq(x1, y1, x2, -1 * y2)))
+
+    dx2 = -1 * s1 * np.divide((y2 - y1), calculate_distance_sq(x1, y1, x2, y2)) \
+          + (-1 * (-1 * s2) * np.divide(1, 2 * y2)) \
+          + (-1 * (-1 * s1) * np.divide((y2 + y1), calculate_distance_sq(x1, -1 * y1, x2, y2)))
+
+    dy2 = 1 * s1 * np.divide((x2 - x1), calculate_distance_sq(x1, y1, x2, y2)) \
+          + ((-1 * s1) * np.divide((x2 - x1), calculate_distance_sq(x1, -1 * y1, x2, y2)))
+    ds1 = 0
+    ds2 = 0
+    grad = np.array([[dx1, dy1, ds1], [dx2, dy2, ds2]])
+
+    return grad
+
+
+def grad_euler_method_improved(system, step=0.0001):
+    x1, y1, s1 = system[0]
+    x2, y2, s2 = system[1]
+    dx1 = -1 * s2 * np.divide((y1 - y2), calculate_distance_sq(x1, y1, x2, y2)) \
+          + (-1 * (-1 * s1) * np.divide(1, 2 * y1)) \
+          + (-1 * (-1 * s2) * np.divide((y1 + y2), calculate_distance_sq(x1, y1, x2, -1 * y2)))
+
+    dy1 = 1 * s2 * np.divide((x1 - x2), calculate_distance_sq(x1, y1, x2, y2)) \
+          + ((-1 * s2) * np.divide((x1 - x2), calculate_distance_sq(x1, y1, x2, -1 * y2)))
+
+    dx2 = -1 * s1 * np.divide((y2 - y1), calculate_distance_sq(x1, y1, x2, y2)) \
+          + (-1 * (-1 * s2) * np.divide(1, 2 * y2)) \
+          + (-1 * (-1 * s1) * np.divide((y2 + y1), calculate_distance_sq(x1, -1 * y1, x2, y2)))
+
+    dy2 = 1 * s1 * np.divide((x2 - x1), calculate_distance_sq(x1, y1, x2, y2)) \
+          + ((-1 * s1) * np.divide((x2 - x1), calculate_distance_sq(x1, -1 * y1, x2, y2)))
+    ds1 = 0
+    ds2 = 0
+    grad_p = np.array([[dx1, dy1, ds1], [dx2, dy2, ds2]])
+
+    x1t = x1 + step * dx1
+    y1t = y1 + step * dy1
+    x2t = x2 + step * dx2
+    y2t = y2 + step * dy2
+
+    dx1t = -1 * s2 * np.divide((y1t - y2t), calculate_distance_sq(x1t, y1t, x2t, y2t)) \
+           + (-1 * (-1 * s1) * np.divide(1, 2 * y1t)) \
+           + (-1 * (-1 * s2) * np.divide((y1t - y2t), calculate_distance_sq(x1t, y1t, x2t, -1 * y2t)))
+    dy1t = 1 * s2 * np.divide((x1t - x2t), calculate_distance_sq(x1t, y1t, x2t, y2t)) \
+           + (-1 * (-1 * s2) * np.divide((x1t - x2t), calculate_distance_sq(x1t, y1t, x2t, -1 * y2t)))
+    dx2t = -1 * s1 * np.divide((y2t - y1t), calculate_distance_sq(x1t, y1t, x2t, y2t)) \
+           + (-1 * (-1 * s2) * np.divide(1, 2 * y2t)) \
+           + (-1 * (-1 * s1) * np.divide((y2t - y1t), calculate_distance_sq(x1t, -1 * y1t, x2t, y2t)))
+    dy2t = 1 * s1 * np.divide((x2t - x1t), calculate_distance_sq(x1t, y1t, x2t, y2t)) \
+           + (-1 * (-1 * s1) * np.divide((x2t - x1t), calculate_distance_sq(x1t, -1 * y1t, x2t, y2t)))
+
+    ds1t = 0
+    ds2t = 0
+
+    grad_t = np.array([[dx1t, dy1t, ds1t], [dx2t, dy2t, ds2t]])
+
+    grad = 1 / 2 * (grad_p + grad_t)
+
+    return grad
+
+
+def update(system, grad, step=0.0001):
+    system = system + step * grad
+    return system
+
+
+def hit_the_boundary(system):
+    y_coord = system[:, 1]
+    for y in y_coord:
+        if y <= 0: return True
+    return False
+
+
+def run_vortex_system(init_system, step=0.0001, epoch=10000, method='euler'):
+    system_history = []
+    system = init_system
+    for i in range(epoch):
+        if method == 'euler_improved':
+            grad = grad_euler_method_improved(system, step)
+        else:  # euler method
+            grad = grad_euler_method(system)
+        system = update(system, grad, step)
+        if hit_the_boundary(system):
+            break
+        system_history.append(system)
+    system_history = np.array(system_history)
+    return np.array(system_history)
+
+
+def solve_system(x1=-0.5, y1=0.2, x2=0.5, y2=1.2, s1=1, lambd=-1, step=0.0003, epoch=8000, method='euler'):
+    s2 = lambd * s1
+    init_system = np.array([[x1,y1, s1],[x2,y2,s2]])
+    history_forward = run_vortex_system(init_system,step,epoch,method)
+    gen_plot_forward_vec(history_forward)
+    history_backward = run_vortex_system(init_system,-1*step,np.int(epoch/4),method)
+    gen_plot_backward_vec(history_backward)
+    bifu_value = calculate_expression(init_system)
+    w = calculate_w(init_system)
+    show_fig_vec(lambd, w,bifu_value)
+    print("$$\lambda$$ : ", lambd)
+    print("Energy : ", calculate_energy_vec(init_system))
+    print("P : ", calculate_p(init_system))
+    print("W : ", w)
+    print("T: ", calculate_t_vec(init_system))
+    print("bifurcation value: ", bifu_value)
+
+# solve_system()
